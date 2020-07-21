ee1b52814cf50789d0676b0e5c1a9ce116813dc6
Javier Sandoval <Javiers2001@live.com>
2020-07-20 20:52:11 -0500

    Project Add

---
+++ Assets/Scripts/City/CityManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CityManager : MonoBehaviour
+{
+
+    
+    private void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            for(int j = 0; j < 20; j ++)
+            {
+                var instance= GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+                instance.transform.localScale =new Vector3(1,Random.Range(1 , 10),1);
+                instance.transform.position = new Vector3(i,0,j);
+            }
+        }
+    }
+
+}
